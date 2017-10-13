9e5f69d996292e02d3b2f10d061eb3b1998a85a3
Matthew Dolan <MatthewDolan@users.noreply.github.com>
2017-10-13 11:15:45 -0700

    Remove extraneous copy-pasta from inc-version (#21)

--- inc-version.sh
+++ inc-version.sh
@@ -11,14 +11,9 @@ CURRENT_VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -
 
 # Increment the minor version
 NEW_VERSION="${CURRENT_VERSION%.*}.$((${CURRENT_VERSION##*.}+1))"
-export NEW_VERSION
 
 # Use maven-help-plugin to update the project.version
 mvn versions:set -DnewVersion=$NEW_VERSION -DgenerateBackupPoms=false
-
-# Update the Version.java class
-VERSION_SOURCE=lightstep-tracer-jre/src/main/java/com/lightstep/tracer/jre/Version.java
-perl -pi -e 's/(\d+)\.(\d+)\.(\d+)/ $ENV{NEW_VERSION} /ge' ${VERSION_SOURCE}
 
 # Commit the changes
 git add common/pom.xml
