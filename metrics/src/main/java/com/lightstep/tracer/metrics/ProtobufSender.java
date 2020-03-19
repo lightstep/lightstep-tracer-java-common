52d92cbfef33687671e1438caf3a9f9d4674a2d2
Seva Safris <seva@safris.org>
2020-03-19 12:47:20 +0700

    Update key names

--- metrics/src/main/java/com/lightstep/tracer/metrics/ProtobufSender.java
+++ metrics/src/main/java/com/lightstep/tracer/metrics/ProtobufSender.java
@@ -45,10 +45,11 @@ class ProtobufSender extends Sender<IngestRequest.Builder,IngestResponse> {
     builder.setDuration(duration);
 
     final KeyValue.Builder componentNameLabel = KeyValue.newBuilder();
-    componentNameLabel.setKey("lightstep.componentName");
+    componentNameLabel.setKey("lightstep.component_name");
     componentNameLabel.setStringValue(componentName);
     builder.addLabels(componentNameLabel);
 
+    // FIXME: Technically, the following line is the proper "java way" to get the hostname. However, this most always returns an internal IP address, which may be incorrect for our needs?!
     final String hostname = InetAddress.getLocalHost().getHostName();
     final KeyValue.Builder hostnameLabel = KeyValue.newBuilder();
     hostnameLabel.setKey("lightstep.hostname");
@@ -56,12 +57,12 @@ class ProtobufSender extends Sender<IngestRequest.Builder,IngestResponse> {
     builder.addLabels(hostnameLabel);
 
     final KeyValue.Builder reporterPlatformLabel = KeyValue.newBuilder();
-    reporterPlatformLabel.setKey("lightstep.reporterPlatform");
+    reporterPlatformLabel.setKey("lightstep.reporter_platform");
     reporterPlatformLabel.setStringValue(getPlatformReporter());
     builder.addLabels(reporterPlatformLabel);
 
     final KeyValue.Builder reporterPlatformVersionLabel = KeyValue.newBuilder();
-    reporterPlatformVersionLabel.setKey("lightstep.reporterPlatformVersion");
+    reporterPlatformVersionLabel.setKey("lightstep.reporter_platform_version");
     reporterPlatformVersionLabel.setStringValue(getPlatformVersion());
     builder.addLabels(reporterPlatformVersionLabel);
 
