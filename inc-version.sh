5e43d12b211fa9c83adf2fcd2a8342b906deff20
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2020-06-23 02:11:59 +0200

    Initial support for release-on-tag. (#142)

--- inc-version.sh
+++ inc-version.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
-
 # To increment the version, use the Makefile
 # make publish
 # which will call out to this script
 
-CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+set -e
 
-# Use maven-help-plugin to get the current project.version
-CURRENT_VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\['`
+if [ "$#" -lt 1 ]; then
+        echo "Increasing minor version automatically"
 
-# Increment the minor version
-NEW_VERSION="${CURRENT_VERSION%.*}.$((${CURRENT_VERSION##*.}+1))"
+        # Use maven-help-plugin to get the current project.version
+        CURRENT_VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v '\['`
+
+        # Increment the minor version
+        NEW_VERSION="${CURRENT_VERSION%.*}.$((${CURRENT_VERSION##*.}+1))"
+else
+        NEW_VERSION=$1
+fi
+
+# Create a branch with the version bump.
+NEW_VERSION_BRANCH="v${NEW_VERSION}_bump"
+git checkout -b $NEW_VERSION_BRANCH
 
 # Use maven-help-plugin to update the project.version
 mvn versions:set -DnewVersion=$NEW_VERSION -DgenerateBackupPoms=false
 
 # Add and commit the changes
 git add common/pom.xml
-git add metrics/pom.xml
 git add grpc/pom.xml
 git add okhttp/pom.xml
 git add bundle-test/pom.xml
@@ -26,4 +34,4 @@ git add example/pom.xml
 git add pom.xml
 
 git commit -m "VERSION $NEW_VERSION"
-git push --set-upstream origin $CURRENT_BRANCH
+git push --set-upstream origin $NEW_VERSION_BRANCH
