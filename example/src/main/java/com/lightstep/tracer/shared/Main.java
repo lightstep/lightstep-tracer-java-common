10337dc305757030b14fb8fd1e1a9dfc2be88857
Konstantin Zhukov <konstantin.zhukov@zalando.de>
2022-04-25 18:50:43 +0200

    modified application to debug 'io.grpc.StatusRuntimeException: UNKNOWN' exception

--- example/src/main/java/com/lightstep/tracer/shared/Main.java
+++ example/src/main/java/com/lightstep/tracer/shared/Main.java
@@ -2,19 +2,30 @@ package com.lightstep.tracer.shared;
 
 import io.opentracing.Span;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String... args) throws Exception {
         ExampleTracer tracer = new ExampleTracer(new Options.OptionsBuilder()
-                .withAccessToken("YOUR_ACCESS_TOKEN")
+                .withCollectorHost(System.getenv("_PLATFORM_OBSERVABILITY_COLLECTOR_ENDPOINT"))
+                .withCollectorPort(8443)
+                .withVerbosity(3)
+                .withComponentName("java-trace-spammer")
+                .withAccessToken(System.getenv("_PLATFORM_OBSERVABILITY_ACCESS_TOKEN"))
                 .build()
         );
 
-        Span span = tracer.buildSpan("parent span").start();
+        while (true){
+            for(int i=0;i<1000;i++){
+                Span span = tracer.buildSpan(String.format("test-span-%d", i)).start();
+                span.setTag("tag-name", "tag-value");
+                span.setTag("tag-name1", "tag-value1");
+                span.setBaggageItem("baggage-item1", "baggage-item-value1");
+                Thread.sleep(3);
+                span.finish();
+            }
 
-        Thread.sleep(1000);
-
-        span.finish();
-
-        tracer.flushInternal(true);
+            tracer.flushInternal(true);
+        }
     }
 }
